cf6e9043a7cd50eccf8a284a00a825c844debc7b
Dan Jacob <danjac354@gmail.com>
2014-08-02 13:11:19 +0300

    Use correct name

--- ownblock/manage.py
+++ ownblock/manage.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kotipalsta.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ownblock.settings.local")
 
     from django.core.management import execute_from_command_line
 
