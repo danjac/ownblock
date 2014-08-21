738111cfa96889befb89b9a219c7c7ddad6e2a72
Dan Jacob <danjac354@gmail.com>
2014-08-21 20:00:34 +0300

    User detail

--- ownblock/ownblock/apps/accounts/views.py
+++ ownblock/ownblock/apps/accounts/views.py
@@ -4,6 +4,11 @@ from rest_framework import status, viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from apps.storage.models import Item
+from apps.parking.models import Vehicle
+from apps.messaging.models import Message
+from apps.notices.models import Notice
+from apps.amenities.models import Booking
 
 from .models import User
 from .serializers import UserSerializer, AuthUserSerializer
@@ -13,6 +18,88 @@ class UserViewSet(viewsets.ModelViewSet):
 
     model = User
     serializer_class = UserSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        data = self.get_serializer(self.object).data
+
+        notices = Notice.objects.filter(author=self.object)
+
+        data['notices'] = []
+
+        for notice in notices.iterator():
+            data['notices'].append({
+                'id': notice.id,
+                'title': notice.title,
+                'details': notice.details,
+                'created': notice.created,
+            })
+
+        if self.object != self.request.user:
+
+            messages = Message.objects.filter(
+                Q(sender=self.object) | Q(recipient=self.object)).filter(
+                Q(sender=self.request.user) | Q(recipient=self.request.user)
+            ).order_by('-created')
+
+            data['sent_messages'] = []
+            data['received_messages'] = []
+
+            for message in messages.iterator():
+                message_data = {
+                    'id': message.id,
+                    'header': message.header,
+                    'details': message.details,
+                    'created': message.created,
+                }
+                if message.sender_id == self.object.id:
+                    data['sent_messages'].append(message_data)
+                else:
+                    data['received_messages'].append(message_data)
+
+        if self.object.role == 'resident':
+            items = Item.objects.filter(resident=self.object)
+
+            data['items'] = []
+
+            for item in items.iterator():
+                data['items'].append({
+                    'id': item.id,
+                    'description': item.description,
+                })
+
+            vehicles = Vehicle.objects.filter(
+                resident=self.object
+            )
+
+            data['vehicles'] = []
+
+            for vehicle in vehicles.iterator():
+                data['vehicles'].append({
+                    'id': vehicle.id,
+                    'description': vehicle.description,
+                    'registration_number': vehicle.registration_number,
+                })
+
+            bookings = Booking.objects.filter(
+                resident=self.object
+            ).select_related('amenity')
+
+            data['bookings'] = []
+            for booking in bookings:
+                data['bookings'].append({
+                                        'id': booking.id,
+                                        'amenity': {
+                                            'id': booking.amenity.id,
+                                            'name': booking.amenity.name,
+                                        },
+
+                                        'reserved_from': booking.reserved_from,
+                                        'reserved_to': booking.reserved_to,
+                                        })
+
+        return Response(data)
 
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs).select_related(
