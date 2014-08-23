2d0f0013c3425add2bd433732d80dfac33c3ce9a
Dan Jacob <danjac354@gmail.com>
2014-08-23 23:18:11 +0300

    Amenity tests

--- ownblock/ownblock/apps/amenities/tests.py
+++ ownblock/ownblock/apps/amenities/tests.py
@@ -1,3 +1,68 @@
+import factory
+
+from unittest.mock import Mock
+
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework import serializers
+
+from factory.django import DjangoModelFactory
+
+from apps.buildings.tests import (
+    BuildingFactory
+)
+
+from .models import Amenity
+from .serializers import BookingSerializer
+
+
+class AmenityFactory(DjangoModelFactory):
+
+    class Meta:
+        model = Amenity
+
+    name = "Sauna"
+    building = factory.SubFactory(BuildingFactory)
+    is_available = True
+
+
+class BookingSerializerTests(TestCase):
+
+    def test_validate_amenity_if_available(self):
+
+        amenity = AmenityFactory(is_available=True)
+        request = Mock()
+        request.building = amenity.building
+
+        attrs = {'amenity': amenity}
+        source = 'amenity'
+        serializer = BookingSerializer(context={'request': request})
+        serializer.validate_amenity(attrs, source)
+
+    def test_validate_amenity_if_not_available(self):
+
+        amenity = AmenityFactory(is_available=False)
+        request = Mock()
+        request.building = amenity.building
+
+        attrs = {'amenity': amenity}
+        source = 'amenity'
+        serializer = BookingSerializer(context={'request': request})
+        self.assertRaises(
+            serializers.ValidationError,
+            serializer.validate_amenity, attrs, source)
+
+    def test_validate_amenity_if_does_not_exist(self):
+
+        amenity = AmenityFactory.create()
+
+        request = Mock()
+        # different building
+        request.building = BuildingFactory.create(address_1='Sesame St')
+
+        attrs = {'amenity': amenity}
+        source = 'amenity'
+        serializer = BookingSerializer(context={'request': request})
+        self.assertRaises(
+            serializers.ValidationError,
+            serializer.validate_amenity, attrs, source)
