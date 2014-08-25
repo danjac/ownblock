03c6bff9305b0111fd7d5af3375d4b35f9d991c8
Dan Jacob <danjac354@gmail.com>
2014-08-25 16:52:45 +0300

    Settings fixes

--- ownblock/ownblock/settings/production.py
+++ ownblock/ownblock/settings/production.py
@@ -45,9 +45,12 @@ SERVER_EMAIL = EMAIL_HOST_USER
 
 # CACHE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = {}
+{
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
 # END CACHE CONFIGURATION
-
 
 # SECRET CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
