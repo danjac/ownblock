c726bb665555207be98cfd6474861562a2c13bc6
Dan Jacob <danjac354@gmail.com>
2014-08-25 18:32:03 +0300

    Using django-dotenv

--- ownblock/manage.py
+++ ownblock/manage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
+
+dotenv.read_dotenv()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ownblock.settings.local")
