bd85db0eea92cc729b560d4bb4694736fcafc923
Dan Jacob <danjac354@gmail.com>
2014-08-26 08:09:04 +0000

    Undo

--- ownblock/ownblock/settings/production.py
+++ ownblock/ownblock/settings/production.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = ['.ownblock.com']
 
 # EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
@@ -44,7 +44,8 @@ EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
 EMAIL_USE_TLS = True
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
-SERVER_EMAIL = EMAIL_HOST_USER
+#SERVER_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
 # END EMAIL CONFIGURATION
 
 
