64dbffd264e5fa1fa3982323b6de9651ad2f77eb
Dan Jacob <danjac354@gmail.com>
2014-08-28 16:54:58 +0300

    Notification email

--- ownblock/ownblock/apps/tickets/views.py
+++ ownblock/ownblock/apps/tickets/views.py
@@ -1,9 +1,17 @@
+from django.conf import settings
 from django.db.models import Q
+from django.template import Context, loader
+from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+
 from rest_framework import viewsets
 
 
 from .models import Ticket
 from .serializers import TicketSerializer, ManagerTicketSerializer
+
+
+User = get_user_model()
 
 
 class TicketViewSet(viewsets.ModelViewSet):
@@ -25,12 +33,44 @@ class TicketViewSet(viewsets.ModelViewSet):
             obj.handler = self.request.user
 
     def post_save(self, obj, created):
-        pass  # TBD: notify managers on create, notify reporter on change
+
+        recipients = User.objects.filter(
+            role='manager',
+            site=obj.building.site
+        )
+
+        if obj.apartment:
+            recipients = recipients | User.objects.filter(
+                role='resident',
+                apartment=obj.apartment,
+            )
+
+        recipients = recipients.exclude(pk=self.request.user.id)
+
+        template = loader.get_template('tickets/emails/notification.txt')
+
+        for recipient in recipients:
+            ctx = Context({
+                'ticket': obj,
+                'created': created,
+                'recipient': recipient,
+                'site': obj.building.site,
+            })
+
+            message = template.render(ctx)
+            subject = '%s: new issue' if created else '%s: issue updated'
+            subject = subject % obj.building.site.name
+
+            send_mail(subject,
+                      message,
+                      settings.DEFAULT_FROM_EMAIL,
+                      [recipient.email])
 
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs).select_related(
             'reporter',
             'apartment',
+            'handler',
             'reporter__apartment',).filter(
             building=self.request.building
         ).order_by('-created')
