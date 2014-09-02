bd6f27b8f96cf6c8e28444608000730cc37fe4cf
Dan Jacob <danjac354@gmail.com>
2014-09-02 16:13:23 +0300

    Added more tests

--- ownblock/ownblock/apps/messaging/tests.py
+++ ownblock/ownblock/apps/messaging/tests.py
@@ -15,6 +15,7 @@ class SerializerTests(TestCase):
     def test_validate_recipient_if_same_as_sender(self):
         apt = ApartmentFactory.create()
         req = Mock()
+        req.building = apt.building
         req.user = ResidentFactory.create(apartment=apt,
                                           email="test@gmail.com")
         serializer = MessageSerializer(context={'request': req})
@@ -26,8 +27,9 @@ class SerializerTests(TestCase):
         bdg = BuildingFactory.create(postcode='387383')
         apt = ApartmentFactory.create(building=bdg)
         req = Mock()
+        req.building = apt.building
         req.user = ResidentFactory.create(apartment=apt)
-        recipient = ResidentFactory.create()
+        recipient = ResidentFactory.create(email="test2@gmail.com")
         serializer = MessageSerializer(context={'request': req})
         attrs = {'recipient': recipient}
         self.assertRaises(serializers.ValidationError,
@@ -37,8 +39,9 @@ class SerializerTests(TestCase):
         bdg = BuildingFactory.create(postcode='387383')
         apt = ApartmentFactory.create(building=bdg)
         req = Mock()
+        req.building = apt.building
         req.user = ResidentFactory.create(apartment=apt)
-        recipient = ResidentFactory.create()
+        recipient = ResidentFactory.create(email="test2@gmail.com")
         serializer = MessageSerializer(context={'request': req})
         attrs = {'recipient': recipient}
         self.assertRaises(serializers.ValidationError,
@@ -47,6 +50,7 @@ class SerializerTests(TestCase):
     def test_validate_recipient_if_all_ok(self):
         apt = ApartmentFactory.create()
         req = Mock()
+        req.building = apt.building
         req.user = ResidentFactory.create(apartment=apt)
         recipient = ResidentFactory.create(apartment=apt,
                                            email='other@gmail.com')
