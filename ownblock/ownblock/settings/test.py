d97e4c3234fcff963fa1757f76edf91e8f91581f
Dan Jacob <danjac354@gmail.com>
2014-09-02 16:07:14 +0300

    Nose support

--- ownblock/ownblock/settings/test.py
+++ ownblock/ownblock/settings/test.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from .base import *
 
-########## IN-MEMORY TEST DATABASE
+# IN-MEMORY TEST DATABASE
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -17,3 +17,9 @@ DATABASES = {
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
 )
+
+INSTALLED_APPS += (
+    'django_nose',
+)
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
