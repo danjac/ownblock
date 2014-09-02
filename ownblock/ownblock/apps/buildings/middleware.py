2a422ff9557316764ed60e915006f512d8a37b64
Dan Jacob <danjac354@gmail.com>
2014-09-02 23:55:20 +0300

    Redirect fix

--- ownblock/ownblock/apps/buildings/middleware.py
+++ ownblock/ownblock/apps/buildings/middleware.py
@@ -1,5 +1,3 @@
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
 from django.utils.functional import SimpleLazyObject
 from django.contrib.sites.models import Site, get_current_site
 
@@ -10,13 +8,7 @@ class CurrentSiteMiddleware(object):
 
     """Gets current Site based on HTTP HOST (fallback on SITE_ID).
 
-    Checks site against logged in user's site (depending on current building).
-    If not matched then redirects user to default site (index page or
-    admin if a staff member).
-
     The site object is added to request.site.
-
-    This middleware goes after CurrentBuildingMiddleware
     """
 
     def get_site(self, request):
@@ -26,31 +18,8 @@ class CurrentSiteMiddleware(object):
         except Site.DoesNotExist:
             return get_current_site(request)
 
-    def get_redirect_url(self, request):
-        # not sure if we really need a redirect here
-        scheme = 'https' if request.is_secure() else 'http'
-        domain = request.site.domain
-        path = None
-
-        if request.building:
-            domain = request.building.site.domain
-            path = request.path
-
-        elif request.user.is_staff:
-            path = reverse('admin:index')
-
-        else:
-            path = reverse('index')
-
-        return '%s://%s%s' % (scheme, domain, path)
-
     def process_request(self, request):
-
         request.site = SimpleLazyObject(lambda: self.get_site(request))
-        if request.site is None or (
-                request.building and request.building.site != request.site):
-            return HttpResponseRedirect(self.get_redirect_url(request))
-        return None
 
 
 class CurrentBuildingMiddleware(object):
