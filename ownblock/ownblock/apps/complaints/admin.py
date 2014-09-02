543e99b8c9bcb68b839ae354022cd1de757dd77c
Dan Jacob <danjac354@gmail.com>
2014-09-02 21:32:38 +0300

    Admin changes

--- ownblock/ownblock/apps/complaints/admin.py
+++ ownblock/ownblock/apps/complaints/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Complaint
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+
+    raw_id_fields = ('building', 'apartment', 'resident')
+    list_display = ('building', 'complaint_summary')
+
+    def complaint_summary(self, obj):
+        return obj.complaint[:50]
+
+
+admin.site.register(Complaint, ComplaintAdmin)
