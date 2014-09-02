0c73d29a14712e6c6fa540a6a51cfe9b986216d4
Dan Jacob <danjac354@gmail.com>
2014-09-02 16:25:36 +0300

    More tests

--- ownblock/ownblock/apps/notices/tests.py
+++ ownblock/ownblock/apps/notices/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from apps.accounts.tests import ManagerFactory
+from apps.buildings.tests import BuildingFactory
+
+from .models import Notice
+
+
+User = get_user_model()
+
+
+class NoticeTests(TestCase):
+
+    def test_has_permission_if_manager(self):
+        building = BuildingFactory()
+        manager = ManagerFactory(site=building.site)
+        notice = Notice(building=building)
+        self.assertTrue(notice.has_permission(manager,
+                                              'notices.change_notice'))
+
+    def test_has_permission_if_author(self):
+        notice = Notice(author=User(role='resident'))
+        self.assertTrue(notice.has_permission(notice.author,
+                                              'notices.change_notice'))
