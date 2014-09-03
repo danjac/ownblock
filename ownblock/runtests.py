6e7dab97f0e9cf036272829ea18d0d29a71ea158
Dan Jacob <danjac354@gmail.com>
2014-09-03 11:54:26 +0300

    Call setup() explicitly

--- ownblock/runtests.py
+++ ownblock/runtests.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import os
 import dotenv
+import django
 
 dotenv.read_dotenv()
 
 if __name__ == "__main__":
 
     os.environ['DJANGO_SETTINGS_MODULE'] = 'ownblock.settings.test'
+    django.setup()
 
     from django.core.management import call_command
 
