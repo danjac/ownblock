d5c10d3e140e19331c0b1d30ebb45c7520f0c298
Dan Jacob <danjac354@gmail.com>
2014-09-05 00:29:54 +0300

    Add analytics to account pages

--- ownblock/fabfile.py
+++ ownblock/fabfile.py
@@ -4,11 +4,10 @@ api.env.hosts = ['178.79.185.15']
 api.env.user = 'deploy'
 api.env.directory = "$HOME/sites/ownblock/ownblock"
 api.env.activate = "source $HOME/.virtualenvs/ownblock/bin/activate"
-api.env.manage = "dotenv %s/manage.py" % api.env.directory
 
 
 def manage(cmd):
-    return api.run("%s %s" % (api.env.manage, cmd))
+    return api.run("dotenv %s/manage.py %s" % (api.env.directory, cmd))
 
 
 @api.task
@@ -23,4 +22,4 @@ def deploy():
         manage("collectstatic --noinput")
         manage("migrate")
 
-        api.sudo("supervisorctl restart webapp")
+    api.sudo("supervisorctl restart webapp")
