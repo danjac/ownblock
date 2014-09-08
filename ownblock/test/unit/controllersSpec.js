b68e3d472f6c81f7eb281ceb943051055b0394e3
Dan Jacob <danjac354@gmail.com>
2014-09-08 16:42:16 +0300

    More js tests

--- ownblock/test/unit/controllersSpec.js
+++ ownblock/test/unit/controllersSpec.js
@@ -12,7 +12,52 @@ describe('controllers', function() {
 
     beforeEach(module('stateMock'));
 
-    it('should maybe show a list of notices', inject(
+    it('should add a new notice', inject(
+        function($rootScope, $controller, $state, _$httpBackend_) {
+            var scope = $rootScope.$new(),
+                httpBackend = _$httpBackend_,
+                state = $state;
+
+            httpBackend.when('POST', '/api/notices/notices').respond('OK');
+
+            $controller('notices.NewCtrl', {
+                $scope: scope
+            });
+
+            scope.save();
+            state.expectTransitionTo('notices.list');
+
+        }
+    ));
+
+    it('should show a single notice', inject(
+        function($rootScope, $controller, _$httpBackend_) {
+
+            var scope = $rootScope.$new(),
+                httpBackend = _$httpBackend_,
+                state = {
+                    params: {
+                        id: 1
+                    }
+                };
+
+            httpBackend.when('GET', '/api/notices/notices/1').
+            respond({
+                id: 1,
+                title: 'test',
+                author: {
+                    full_name: 'tester'
+                }
+            });
+            $controller('notices.DetailCtrl', {
+                $scope: scope,
+                $state: state
+            });
+            httpBackend.flush();
+            expect(scope.notice.title).toBe('test');
+        }));
+
+    it('should show a list of notices', inject(
         function($rootScope,
             $controller, _$httpBackend_) {
             var scope = $rootScope.$new(),
